4316bfed258edfab0958bee3a9a7c396481142e1
Crystina <x978zhan@uwaterloo.ca>
2021-05-31 21:46:57 -0400

    clean up: group train / dev files into language

---
+++ prep_data_from_tydi/0_categorized_tydi_data.sh
@@ -0,0 +1,15 @@
+tydi_dir=$1
+tydi_has_pos_label_dir="${tydi_dir}/with_answer"
+tydi_has_no_pos_label_dir="${tydi_dir}/without_answer"
+
+python get_lines_with_answers.py $tydi_dir 
+
+for set_name in 'train' 'dev'
+do	
+	for lang in thai swahili telugu finnish bengali russian japanese arabic indonesian korean english
+	do
+		echo "Extracting ${lang}..." 
+		cat "${tydi_has_pos_label_dir}/tydiqa-v1.0-${set_name}.jsonl" | grep  "\"language\":\"${lang}\"" > "${tydi_has_pos_label_dir}/tydiqa-v1.0-${set_name}.${lang}.jsonl" 
+		cat "${tydi_has_no_pos_label_dir}/tydiqa-v1.0-${set_name}.jsonl" | grep  "\"language\":\"${lang}\"" > "${tydi_has_no_pos_label_dir}/tydiqa-v1.0-${set_name}.${lang}.jsonl" 
+	done
+done
