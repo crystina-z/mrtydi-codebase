1c46cb262348bea9277dc34f357521e5b383c8f3
Crystina <xzhangbx@ust.hk>
2021-06-01 10:58:23 -0400

    remove outdated files

--- prep_data_from_tydi/srun.sh
+++
@@ -1,2 +0,0 @@
-srun --nodes 1 --mem=120GB --time=0-3:00 --gres=gpu:v100:1 --cpus-per-task=8 --pty bash
-# srun --nodes 1 --mem=120GB --time=0-3:00  --cpus-per-task=8 --account=rrg-jimmylin --pty bash 
