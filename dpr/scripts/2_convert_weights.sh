6914e5cf1079e6f336dfd89aaf33fcf27602bc3e
Crystina <x978zhan@uwaterloo.ca>
2021-07-27 00:03:00 +0200

    add dpr scripts

---
+++ dpr/scripts/2_convert_weights.sh
@@ -0,0 +1,4 @@
+python dpr/tools/convert_weights.py \
+    -q GC-DPR/outputs/dpr_biencoder.2.16721 \
+    -c GC-DPR/outputs/dpr_biencoder.2.16721 \
+    -o GC-DPR/outputs/hf_format
---
+++ dpr/scripts/3_encode_collection.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH -p gpu20
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes 1
+#SBATCH --gres gpu:4
+#SBATCH --time 12:00:00
+#SBATCH --output outputs/index-search.dpr-mbert-uncased.ft-on-mrtydi.bengali.log
+
+# cd pyserini
+
+nvidia-smi
+source /GW/NeuralIR/work/cuda-10.1_env.sh
+
+lang=bengali
+
+hf_model_dir="./models/hf-models"
+mrtydi_data_dir="/GW/carpet/nobackup/czhang/dpr/data/mrtydi"
+test_topic_fn="${mrtydi_data_dir}/ori/${lang}/topic.test.tsv"
+coll_json_dir="${mrtydi_data_dir}/test/${lang}_collection_jsonl"
+
+# files to output
+index_dir="${mrtydi_data_dir}/faiss_index/$lang"
+runfile="runs/${lang}.run.mdpr.mrtydi-test/trec"
+
+
+# 1. index
+if [ ! -d $index_dir ]; then
+    mkdir -p $index_dir/..
+    python -m pyserini.dindex \
+        --encoder "$hf_model_dir/mdpr-context-encoder" \
+        --corpus $coll_json_dir \
+        --index $index_dir
+else
+    echo "Found existing ${index_dir}, skip."
+fi
+
+
+# 2. search
+if [ ! -f $runfile ]; then
+    python -m pyserini.dsearch \
+        --topics $test_topic_fn \
+        --index $index_dir \
+        --encoder "$hf_model_dir/mdpr-question-encoder" \
+        --output $runfile \
+        --batch-size 36 --threads 12
+else
+    echo "Found existing ${runfile}, skip."
+fi
+
+
+# cd ..
