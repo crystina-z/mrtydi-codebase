d62ea91cb6d4f8626d7768e5bbf816a19d88a1c0
Crystina <x978zhan@uwaterloo.ca>
2021-07-29 01:08:49 +0200

    plot reletive score: add bar score; add y-axis label; fix x position

--- analysis/plot_relative_score_bars.py
+++ analysis/plot_relative_score_bars.py
@@ -12,8 +12,13 @@ colors = ["tab:blue", "tab:orange", "tab:green", "tab:pink", "tab:purple"]
 scores = pd.read_csv("stats/scores.tsv", delimiter="\t", header=0)
 n_rows = len(scores)
 
+
+def filter_fn(df):
+    return (df != "BM25") & (df != "BM25-tuned")
+
 methods = scores["method"]
-valid_methods = methods[methods != "BM25"]
+# valid_methods = methods[methods != "BM25"]
+valid_methods = methods[filter_fn(methods)]
 n_methods = len(valid_methods)
 method2color = dict(zip(valid_methods, colors))
 
@@ -26,26 +31,31 @@ for lang in scores:
     langs.append(lang)
     xs.append(i)
 
-    lang_scores = scores[lang][methods != "BM25"]
+    # lang_scores = scores[lang][methods != "BM25"]
     bm25 = scores[lang][methods == "BM25-tuned"].item()
-    x_start = i - (bar_width * n_methods / 2)
+    lang_scores = scores[lang][filter_fn(methods)]
+
+    x_start = i - (bar_width * (n_methods - 1) / 2)
 
     for j, (method, score) in enumerate(zip(valid_methods, lang_scores)):
-        # color = 
-        rel_score = score / bm25
+        rel_score = round(score / bm25, 2)
 
         cur_x = x_start + bar_width * j
         edge_color = method2color[method]
         face_color = (*mat_colors.to_rgba(edge_color)[:3], 0.6)
 
         if i == 0:
-            plt.bar(
-                x=cur_x, width=bar_width, height=rel_score, color=face_color, edgecolor=edge_color, label=method)
+            rects = plt.bar(x=cur_x, width=bar_width, height=rel_score, color=face_color, edgecolor=edge_color, label=method)
         else:
-            plt.bar(
-                x=cur_x, width=bar_width, height=rel_score, color=face_color, edgecolor=edge_color)
+            rects = plt.bar(x=cur_x, width=bar_width, height=rel_score, color=face_color, edgecolor=edge_color)
+        plt.bar_label(rects, padding=1)
 
     i += (bar_width * n_methods + distance)
+
+
+plt.plot(
+    [xs[0] - bar_width * n_methods, xs[-1] + bar_width],
+    [1, 1], label="BM25 (tuned)", linestyle="--", color="tab:grey")
 
 
 file_dir = os.path.dirname(__file__) 
@@ -53,5 +63,6 @@ plot_dir = os.path.join(file_dir, "plots")
 
 plt.legend()
 plt.xticks(xs, langs)
+plt.ylabel("Reletive MRR comparing to tuned BM25")
 plt.tight_layout()
 plt.savefig(f"{plot_dir}/relative_score_bars.png")
