a4a80d5c8f7b0fae54d1be4bdfcb5cadce274825
Crystina <x978zhan@uwaterloo.ca>
2021-08-06 15:23:35 +0200

    training script; support dev set validation

--- dpr/scripts/1_train_mdpr.sh
+++ dpr/scripts/1_train_mdpr.sh
@@ -1,14 +1,26 @@
-set -e
+#!/bin/bash
+#SBATCH -p gpu20
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes 1
+#SBATCH --gres gpu:4
+#SBATCH --time 48:00:00
+#SBATCH --output outputs/dpr-mbert-cased.with-validation.train.log
+
+nvidia-smi
+source /GW/NeuralIR/work/cuda-10.1_env.sh
+
+# set -e
 
 cd GC-DPR
 
-pretrain_model=bert-base-multilingual-uncased
+# pretrain_model=bert-base-multilingual-uncased
+pretrain_model=bert-base-multilingual-cased
 
 python train_dense_encoder.py \
    --encoder_model_type hf_bert \
    --pretrained_model_cfg $pretrain_model \
-   --train_file "dpr_data/data/mrtydi/*json" \
-   --dev_file "dpr_data/data/mrtydi/*json" \
+   --train_file "dpr_data/data/mrtydi/train/*train.json" \
+   --dev_file "dpr_data/data/mrtydi/dev/*dev.json" \
    --output_dir outputs \
    --grad_cache \
    --q_chunk_size 16 \
