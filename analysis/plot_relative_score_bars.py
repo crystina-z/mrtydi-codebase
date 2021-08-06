04ae7ede197dcdaaa8c106dbae646c08f6586bf6
Crystina <x978zhan@uwaterloo.ca>
2021-08-06 18:25:25 +0200

    plot scatter for relative score

--- analysis/plot_relative_score_bars.py
+++ analysis/plot_relative_score_bars.py
@@ -57,7 +57,6 @@ def barplot():
         [xs[0] - bar_width * n_methods, xs[-1] + bar_width],
         [1, 1], label="BM25 (tuned)", linestyle="--", color="tab:grey")
 
-
     file_dir = os.path.dirname(__file__) 
     plot_dir = os.path.join(file_dir, "plots")
 
@@ -68,5 +67,56 @@ def barplot():
     plt.savefig(f"{plot_dir}/relative_score_bars.png")
 
 
+def scatter():
+    i = 0
+    xs, langs = [], []
+
+    # lang 2 all score
+    lang2all_scores = {lang: {
+        "bm25": scores[lang][methods == "BM25-tuned"].item(),
+        "mDPR": scores[lang][methods == "mDPR"].item(),
+        "hybrid": scores[lang][methods == "hybrid"].item(),
+    } for lang in scores if lang != "method"}
+    lang2rel_scores = {lang: {
+        method: score / scores["bm25"] for method, score in scores.items()
+    } for lang, scores in lang2all_scores.items()} 
+
+    # sort according to relevant mdpr score
+    lang_rel_scores = sorted(
+        lang2rel_scores.items(), 
+        key=lambda kv: (kv[1]["mDPR"], kv[1]["hybrid"])
+    )
+
+    # plot
+    for i, (lang, rel_scores) in enumerate(lang_rel_scores):
+        for method in ["mDPR", "hybrid"]:
+            common_kwargs = {
+                "color": method2color[method],
+                "s": 5,
+            }
+            if i == 0:
+                plt.scatter(i, rel_scores[method], label=method, **common_kwargs)
+            else:
+                plt.scatter(i, rel_scores[method], **common_kwargs)
+            plt.text(s=lang, x=i + 0.1, y=rel_scores[method])
+
+    xticks, _ = plt.xticks()
+    # print(xticks[0], xticks[-1])
+    plt.plot(
+        [-0.5, len(lang_rel_scores) - 0.5], [1, 1],
+        color="tab:grey", label="BM25 (tuned)", linestyle="--",    
+    ) 
+ 
+    file_dir = os.path.dirname(__file__) 
+    plot_dir = os.path.join(file_dir, "plots")
+
+    plt.legend()
+    # plt.xticks(xs, langs)
+    plt.ylabel("Relative MRR comparing to tuned BM25")
+    plt.tight_layout()
+    plt.savefig(f"{plot_dir}/relative_score_scatter.png")
+
+
 if __name__ == "__main__":
-    barplot()
+    # barplot()
+    scatter()
