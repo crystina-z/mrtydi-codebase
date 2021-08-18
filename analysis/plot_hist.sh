3cfe6366a82e461b90ea119046ec8f1c503dd8ae
Xinyu Crystina ZHANG <x978zhan@uwaterloo.ca>
2021-08-18 09:12:30 -0400

    change path

--- analysis/plot_hist.sh
+++ analysis/plot_hist.sh
@@ -7,18 +7,19 @@ do
     echo $lang
     echo "========================================"
 
+    bm25_fn=${results_dir}/bm25/runs-top100/$lang/bm25.test.k1*.b*
+
     python hist_of_ranks.py \
 	-l $lang --tag dense \
 	-q ${dataset_dir}/${lang}/qrels.test.txt \
-	-bm25 ${results_dir}/bm25/runs/$lang/bm25.test.k1*.b* \
-	-mdpr ${results_dir}/mdpr/runs/$lang/test.trec
+	-bm25 ${bm25_fn} \
+	-mdpr ${results_dir}/mdpr/runs-top100/${lang}/test.trec*
 
     python hist_of_ranks.py \
 	-l $lang --tag hybrid \
 	-q ${dataset_dir}/${lang}/qrels.test.txt \
-	-bm25 ${results_dir}/bm25/runs/$lang/bm25.test.k1*.b* \
-	-mdpr ${results_dir}/hybrid/run.hybrid.test.${lang}.trec 
+	-bm25 ${bm25_fn} \
+	-mdpr ${results_dir}/hybrid/runs-top100/${lang}/run.hybrid.test.${lang}.trec*
 
     echo 
 done
-
