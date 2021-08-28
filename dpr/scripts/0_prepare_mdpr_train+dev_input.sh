0b2d6e824d17f4b8f54aa87abb68e5adac0c5474
Crystina <x978zhan@uwaterloo.ca>
2021-08-28 16:32:58 +0200

    add script for post-process data

--- dpr/scripts/0_prepare_mdpr_train+dev_input.sh
+++ dpr/scripts/0_prepare_mdpr_train+dev_input.sh
@@ -7,3 +7,8 @@ do
         echo $lang
         python tools/generate_dpr_json.py $mr_tydi_dir $output_dir $lang
 done
+
+
+# for post-processing:
+python tools/print_dpr_input_size.py $output_dir
+python tools/sample_dpr_input_size.py $output_dir
